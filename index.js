4d0c04cab39fbfd622b5e45f18ed9d93d329d9b3
jumana-nounou <jumana.nounou@student.giu-uni.de>
2022-07-19 17:19:16 +0200

    init

--- index.js
+++ index.js
@@ -14,96 +14,60 @@ app.all('*', function(req, res, next) {
   next();
 });
 
-//Y4CBPCTEBXA8YCPJLCFG97ZT8 visual crossing
-//832e32cdeb88416bbb1143919220807 weather api
-//02744ae4a869785d1a2a98ab4f81d09e ipstack
-
-const accesskeyVC='Y4CBPCTEBXA8YCPJLCFG97ZT8';
-
-
-//https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/New%20York%20City%2CNY?unitGroup=us&forecastDays=5&iconSet=icons1&key=Y4CBPCTEBXA8YCPJLCFG97ZT8&contentType=json
-//?locations=Cairo&forecastDays=5&iconSet=icons1&contentType=json&key=${access_key}
+const accesskeyIP="02744ae4a869785d1a2a98ab4f81d09e";
+const openkey=process.env.openWeatherkey;
 
 
 //Api that gets the current weather of the current location
 app.get('/weather', async (req,res) => {
-  
-const params = {
-  key: accesskeyVC,
-  latitude:req.body.latitude,
-  longitude:req.body.longitude,
-  forecastDays : '7',
-  contentType : 'json',
-  aggregateHours:24,
-  iconSet :'icons1'
-}
-
-const { data } =  await axios.get("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${latitude},${longitude}",{params}).catch(error => {
+ const params={
+    lat:req.body.lat,
+    lon:req.body.lon,
+    APIKey:openkey
+ 
+ };
+const { data } =  await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${params.lat}&lon=${params.lon}&appid=${params.APIKey}&exclude=minutely&units=metric`,{params}).catch(error => {
   console.log(error);
 });
-processWeatherData(data);
-
+ processWeatherData(data);
+console.log(data);
 return res.send(data);
 });
 
-// api that gets the weather when entering a specific city
-app.get('/weather', async (req,res) => {
-  
-  const params = {
-    key: accesskeyVC,
-    location :req.body,
-    forecastDays : '7',
-    contentType : 'json',
-    aggregateHours:24,
-    iconSet :'icons1'
-  }
-  
-    const { data } =  await axios.get("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${latitude},${longitude}",{params}).catch(error => {
-          console.log(error);
-        });
-    processWeatherData(data);
-  
-   return res.send(data);
-  });
 
 
-  function processWeatherData(response) {
-  
-    var location=response.resolvedAddress;
-    var days=response.days;
-    console.log("Location: "+location);
-    for (var i=0;i<days.length;i++) {
-      console.log(days[i].datetime+": tempmax="+days[i].tempmax+", tempmin="+days[i].tempmin);
-    }
-  }
+app.get('/location', async (req,res) => {
+  const { data } =  await axios.get('https://geo.ipify.org/api/v2/country,city?apiKey=at_pZdMvxABS0ZniTVyqQ9kVBbLT9QDQ');
+  processLocation(data);
+ return res.send(data);
 
-  // api that gets the location 
- app.get('/location', async (req,res) => {
-   const { data } =  await axios.get('https://geo.ipify.org/api/v2/country,city?apiKey=at_pZdMvxABS0ZniTVyqQ9kVBbLT9QDQ');
-   
-  return res.send(data);
 });
  
+function processLocation(res){
+  var location=res.location.city;
+  var lat=res.location.lat;
+  var lng=res.location.lng;
+  console.log("location:"+location);
+  console.log("latitude is: "+lat);
+  console.log("longitude is: "+lng);
+}
+function processWeatherData(response) {
+  
+  var timezone=response.timezone;
+  var current=response.current;
+  console.log("Timezone: "+timezone);
+
+  for (var i=0;i<current.length;i++) {
+    console.log("Current weather: "+current[i].temp);
+  }
+}
+
+
+
+app.listen(5000);
 
 
 
 
-
-
-
-
-
-
-
-
-app.get('/api', async (req,res) => {
-
-  const { data } =  await axios.get('https://api64.ipify.org?format=json');
-
-  return res.send(data);
-});
-
-
-
-
-app.listen(3000);
+//https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/New%20York%20City%2CNY?unitGroup=us&forecastDays=5&iconSet=icons1&key=Y4CBPCTEBXA8YCPJLCFG97ZT8&contentType=json
+//?locations=Cairo&forecastDays=5&iconSet=icons1&contentType=json&key=${access_key}
